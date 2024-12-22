3da19469aeb22efa3c9bfe5d468da8bdc8cb0b70
FinichMohamed <mfinich@gmail.com>
2024-12-22 13:08:24 +0100

    Initialisation du projet Spring Boot avec Web, Data JPA, et H2

---
+++ src/main/java/com/bpifrance/capitaldetection/BpifranceCapitalAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.bpifrance.capitaldetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpifranceCapitalAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BpifranceCapitalAnalysisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bpifrance/capitaldetection/BpifranceCapitalAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bpifrance.capitaldetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpifranceCapitalAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
